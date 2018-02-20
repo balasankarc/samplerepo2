24eff347c8d29bb186efea53e6bfb496a40aed8d
Balasankar C <balasankar@gitlab.com>
2018-02-20 05:50:24 +0000

    Update code.rb

--- code.rb
+++ code.rb
@@ -4,3 +4,13 @@ system("sudo apt-get -y install gitlab-ee=10.3.0-ee.0")
 system("/opt/gitlab/embedded/bin/runsvdir-start &")
 system("sudo gitlab-ctl reconfigure")
 system("sudo apt-get -y install gitlab-ee=10.4.0-ee.0")
+log_location = "/var/log/apt/term.log"
+system("tac #{log_location} | sed '/^Log started/q' | tac > /tmp/upgrade.log")
+start_string = File.open("/tmp/upgrade.log").grep(/Log started/)[0].strip.gsub("Log started: ", "")
+end_string = File.open("/tmp/upgrade.log").grep(/Log ended/)[0].strip.gsub("Log ended: ", "")
+start_time = DateTime.strptime(start_string, "%Y-%m-%d  %H:%M:%S")
+end_time = DateTime.strptime(end_string, "%Y-%m-%d  %H:%M:%S")
+duration_in_secs = (end_time - start_time) * 24 * 60 * 60
+item = "10.4.0-ee.0, #{duration_in_secs}"
+
+puts item
