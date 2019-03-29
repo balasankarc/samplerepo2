7f0a2e335f24f12e8fe3714e10fe9ff3506f79b1
Balasankar "Balu" C <balasankar@gitlab.com>
2019-03-29 17:10:37 +0530

    Try restricted output
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- test.rb
+++ test.rb
@@ -6,4 +6,12 @@ class Lorem
   end
 end
 
-Lorem.hello("Balu")
+def restricted_output
+  puts "section_start:#{Time.now.to_i}:chat_reply\r\033[0K"
+
+  yield
+ensure
+  puts "section_end:#{Time.now.to_i}:chat_reply\r\033[0K"
+end
+
+resitrcted_output { Lorem.hello("Balu") }
